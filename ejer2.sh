be7ac1a736e49cafd9f5b2662b93399944e4b93d
graciela <graciela@graciela10.(none)>
2019-03-09 13:40:53 +0100

    Añadi el ejer2 corregido

--- ejer2.sh
+++ ejer2.sh
@@ -1,6 +1,11 @@
+#Estaba mal la parte de hacer un bucle de cuando el numero no sea correcto que lo volviera a pedir, la manera correcta:
 read -p "Dime un numero: " valor
 
-while [ $valor -gt 0 ]; do
+while [ $valor -le 0 ]; do
+echo "Numero no valido"
+read -p "Introduce un valor: " valor
+done
+
 if [ $((valor%2)) -eq 0 ]; then
 echo "Es par"
 valor=0
@@ -8,4 +13,4 @@ else
 echo "Es impar"
 valor=0
 fi
-done
+
