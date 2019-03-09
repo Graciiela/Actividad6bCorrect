5b402786add8013e7062cd34a679606988cb131d
graciela <graciela@graciela10.(none)>
2019-03-09 13:41:17 +0100

    Añadi el ejer3 corregido

--- ejer3.sh
+++ ejer3.sh
@@ -1,5 +1,10 @@
 clear
+#Faltaba poner el bucle de que lo vuelva a pedir el numero si no es correcto
 read -p "Dime una nota " nota
+
+while [ $nota -le 0 ] || [ $nota -gt 10 ]; do
+read -p "Dime una nota de 0 a 10: " nota
+done
 
 if [ $nota -gt 0 ] && [ $nota -lt 4 ];then
 	echo "Suspendido"
@@ -13,9 +18,8 @@ elif [ $nota -eq 8 ];then
 	echo "Notable"
 elif [ $nota -eq 9 ];then
 	echo "Sobresaliente"
-elif [ $nota -eq 10 ];then
+else 
 	echo "Sobresaliente"
-else
-	echo "No se puede evaluar"
+
 fi
 
